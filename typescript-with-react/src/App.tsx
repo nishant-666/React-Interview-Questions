8a443cb8398c1ae85f44f1ec1ac503967831758c
Nishant <nishants440@gmail.com>
2022-08-17 10:09:31 +0530

    Adding Typescript to React

---
+++ typescript-with-react/src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  const user = {
+    firstName: "Angela",
+    lastName: "Davis",
+    role: "Professor",
+  };
+
+  console.log(user.firstName);
+  return <div className="App"></div>;
+}
+
+export default App;
